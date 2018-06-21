50ffff8b911eda024d5a30edc6555867256084d8
libelula70 <anelen70@gmail.com>
2018-06-21 13:58:03 +0200

    js

---
+++ 01_Datos/Hola_Funcion.js
@@ -0,0 +1,14 @@
+function saludar(user) {
+    const SALUDO='Hola'
+    console.log(SALUDO, user)
+}
+
+saludar('Pepe')
+saludar('Elena')
+
+function sumar(a,b) {
+    let resultado=a+b
+    console.log(resultado)
+}
+
+sumar(2,4)
---
+++ 01_Datos/Hola_Mundo.js
@@ -0,0 +1,4 @@
+var saludo
+saludo="Hola"
+var user="Mundo"
+console.log(saludo, user)
