9ee38e9ebdbc6fb71e9f0e757e721413d045214d
libelula70 <anelen70@gmail.com>
2018-06-29 12:19:50 +0200

    despertador y saludo

--- 03_expresiones/operadores.js
+++ 03_expresiones/operadores.js
@@ -1,34 +1,96 @@
-let x=23
-let y
-y=x++ //x=x+1 y luego y=x
-y=++x //y=x y luego x=x+1
-console.log(x)
-/* console.log(x++) */
-console.log(++x)
-
-
-
+function incrementos () {
+	let x = 23
+	let y
+	y = ++x // x= x+1 y luego y=x
+	y = x++ // y=x y luego x= x+1 
+	/* console.log(x++)
+	console.log(x) */
+	y =--x // x= x-1 y luego y=x
+	y = x-- // y=x y luego x= x-1 
+}
 
 function asignaciones () {
-    let x=2
-    x+=1  //x=x+1
+	let x = 2
+	x += 1 // x = x + 1
+	x = 2
+	x -= 1
+	x = 2
+	x *= 3
+	x = 2
+	x /= 2
+	x = 2
+	x %= 2
 }
 
-function esPar (a) {
-    let isPar=true
-    if (a % 2) {
-        isPar=false
-    }
-    return isPar
+/* function esPar (num) {
+	let isPar = true
+	if (typeof num != 'number'|| (num % 2)) {
+		isPar = false
+	} 
+	return isPar
 }
 
-console.log(22, esPar(22))
-console.log(25, esPar(25))
+function esPar2 (num) {
+	let isPar = false
+	// compruebo si SI eres par: num % 2 == 0
+	if (!num % 2) {
+		isPar = true
+	} 
+	return isPar
+}
 
-console.log(Boolean(22))
 
-//falsy:
-false
-0
-''
-undefined
+
+// incrementos()
+// asignaciones()
+
+console.log('22 es par?', esPar(22))
+console.log('25 es par?', esPar(25))
+console.log('0 es par?', esPar(0))
+console.log('Pepe es par?', esPar('Pepe'))
+console.log('"" es par?', esPar(''))
+console.log(' es par?', esPar())
+console.log('true es par?', esPar(true))
+console.log('false es par?', esPar(false)) */
+
+
+
+// falsy: 
+/* console.log(Boolean(false))
+console.log(Boolean(0))
+console.log(Boolean(''))
+console.log(Boolean(undefined)) */
+// truly
+/* console.log(Boolean(true))
+console.log(Boolean(109))
+console.log(Boolean('Pepe'))
+ */
+/* let a = 'Juan'
+let b = 0
+// compruebo si b == 0
+if (!b) {
+	a = 'Pepe'
+}
+console.log(a)
+b = 4
+// compruebo si b != 0
+if (b) {
+	a = 'Luis'
+}
+console.log(a)
+ */
+
+
+function esParFinal (num) {
+	return (typeof num == 'number' && num % 2 ==0) ? true : false
+}
+
+
+console.log('22 es par?', esParFinal(22))
+console.log('25 es par?', esParFinal(25))
+console.log('0 es par?', esParFinal(0))
+console.log('Pepe es par?', esParFinal('Pepe'))
+console.log('"" es par?', esParFinal(''))
+console.log(' es par?', esParFinal())
+console.log('true es par?', esParFinal(true))
+console.log('false es par?', esParFinal(false))
---
+++ 03_expresiones/semana.js
@@ -0,0 +1,109 @@
+/**
+*
+*@param 'Día de la semana'
+*@return 'Hora de la alarma' -> 7:00 // 12:00
+            y "comienza el <dia>"
+*
+*/
+
+function despertador (dia) {
+    let hora = '12:00'
+    
+    if (dia=='L') {
+        hora= '7:00'
+        dia='lunes'
+    }
+    else if (dia=='M') {
+        hora= '7:00'
+        dia='martes'
+    }
+    else if (dia=='X') {
+        hora= '7:00'
+        dia='miércoles'
+    }
+    else if (dia=='J') {
+        hora= '7:00'
+        dia='jueves'
+    }
+    else if (dia=='V') {
+        hora= '7:00'
+        dia='viernes'
+    }
+    else if (dia=='S') {
+        hora= '12:00'
+        dia='sábado'
+    }else if (dia=='D') {
+        hora= '12:00'
+        dia='domingo'
+    }
+        hora='7:00'
+
+    return `${hora}, comienza el ${dia}`;
+}
+
+function probar () {
+    let dia='L'
+    console.log(`Si tu día es ${dia}`, despertador (dia)) // 7:00
+    dia='M'
+    console.log(`Si tu día es ${dia}`, despertador (dia)) // 7:00
+    dia='S'
+    console.log(`Si tu día es ${dia}`, despertador (dia)) // 7:00
+    dia='D'
+    console.log(`Si tu día es ${dia}`, despertador (dia)) // 7:00
+}
+
+probar ()
+
+
+function despertador(dia) {
+    let hora = '12:00'
+    switch (dia) {
+        case 'L':
+            hora = '7:00'
+            dia = 'Lunes'
+            break;
+        case 'M':
+            hora = '7:00'
+            dia = 'Martes'
+            break;
+        case 'X': 
+            hora = '7:00'
+            dia = 'Miercoles'
+            break;
+        case 'J': 
+            hora = '7:00'
+            dia = 'Jueves'
+            break;
+        case 'V' :
+            hora = '7:00'
+            dia = 'Viernes'
+            break;
+        case 'S' :
+            hora = '12:00'
+            dia = 'Sábado'
+            break;
+        case 'D':
+            hora = '12:00'
+            dia = 'Domingo'  
+            break;
+        default:
+            hora = '0:00'
+            dia = "Error de día"
+    }
+    return `${hora}, comenza el ${dia}`
+}
+
+function probar () {
+    let dia = 'L'
+    console.log(`Si tu dia es ${dia}`, despertador(dia)) // 7:00
+    dia = 'M'
+    console.log(`Si tu dia es ${dia}`, despertador(dia)) // 7:00
+    dia = 'S'
+    console.log(`Si tu dia es ${dia}`, despertador(dia)) // 12:00
+    dia = 'D'
+    console.log(`Si tu dia es ${dia}`, despertador(dia)) // 12:00
+    dia = 'Z'
+    console.log(`Si tu dia es ${dia}`, despertador(dia)) // 12:00
+}
+
+probar()
