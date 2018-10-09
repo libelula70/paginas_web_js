ba1cc5e8b8307fa8a25fefb9fa58d0d51c4c99a0
libelula70 <anelen70@gmail.com>
2018-10-09 18:59:06 +0200

    Scroll en proyecto Italia

---
+++ 00_Proyecto/js/app.js
@@ -0,0 +1,27 @@
+class App {
+
+    constructor () {
+        this.btnSubir = document.querySelector('#btn-subir')
+        this.divSubir = document.querySelector('#div-subir')
+    
+        document.addEventListener('scroll', this.scollDetect.bind(this) )
+        this.btnSubir.addEventListener('click', this.subir.bind(this))
+    }   
+    
+    scollDetect (oE) {
+        console.log (oE.target.scrollingElement.scrollTop)
+        if (oE.target.scrollingElement.scrollTop > 200) {
+            this.divSubir.classList.toggle ('hide')
+        } 
+    }  
+
+    subir(oE) {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => new App())
